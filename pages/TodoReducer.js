057ab38775809d095af47c6b4ee7f81efd7ba24e
Jeremy Hong <jeremy.hong@hp.com>
2021-11-28 16:56:36 +0800

    Add Reducer version

---
+++ pages/TodoReducer.js
@@ -0,0 +1,83 @@
+import { useEffect, useReducer } from 'react'
+import HighlightButton from '../components/HighlightButton'
+
+const initState = {
+  list: [{ checked: false, name: 'init' }],
+  name: '',
+  mode: 'All',
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+  case 'UpdateName':
+    return { ...state, name: action.name }
+  case 'UpdateMode':
+    return { ...state, mode: action.mode }
+  case 'UpdateList':
+    return { ...state, list: action.list }
+  default:
+    throw new Error()
+  }
+}
+
+export default function TodoReducer() {
+  const [state, dispatch] = useReducer(reducer, initState)
+  const [list, setList] = [state.list, (x) => dispatch({ type: 'UpdateList', list: x(state.list) })]
+  const [name, setName] = [state.name, (x) => dispatch({ type: 'UpdateName', name: x })]
+  const [mode, setMode] = [state.mode, (x) => dispatch({ type: 'UpdateMode', mode: x })]
+
+  useEffect(() => {
+    console.log(state)
+  }, [state.list])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setList((prev) => [...prev, { checked: false, name }])
+    setName('')
+  }
+  const handleDelete = (i) => {
+    setList((prev) => {
+      prev.splice(i, 1)
+      return [...prev]
+    })
+  }
+  const handleCheck = (e, i) => {
+    setList((prev) => {
+      prev[i].checked = e.target.checked
+      return [...prev]
+    })
+  }
+  function TodoItem({ todo, i }) {
+    return (
+      <li>
+        <input type='checkbox' onClick={(e)=>handleCheck(e, i)} checked={todo.checked} onChange={()=>{}} />
+        {todo.name}
+        <button onClick={()=>handleDelete(i)}>Delete</button>
+      </li>
+    )
+  }
+  function TodoList({ mode, list }) {
+    return (<ul>
+      {list.map((todo, i) => (
+        (mode === 'All' || mode === 'Checked' && todo.checked || mode === 'Unchecked' && !todo.checked) ?
+          <TodoItem key={i} todo={todo} i={i} /> :
+          <div key={i}></div>
+      ))}
+    </ul>)
+  }
+
+  return (
+    <div name='container'>
+      <h2>Todo State Reducer</h2>
+      <input onChange={(e) => setName(e.target.value)} value={name}/>
+      <button onClick={handleSubmit}>Add</button>
+      <br/>
+      <HighlightButton setMode={setMode} mode={mode} value='All' />
+      <HighlightButton setMode={setMode} mode={mode} value='Checked' />
+      <HighlightButton setMode={setMode} mode={mode} value='Unchecked' />
+      <button onClick={()=>console.log(JSON.stringify(list, null, 2))}>Debug</button>
+      <hr></hr>
+      <TodoList mode={mode} list={list} />
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,29 @@ import Head from 'next/head'
 import { useState } from 'react'
 import TodoState from './TodoState'
 import TodoStateApi from './TodoStateApi'
+import TodoReducer from './TodoReducer'
 import HighlightButton from '../components/HighlightButton'
 import { ToastContainer, Slide } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 export default function Home() {
-  const [page, setPage] = useState('StateApi')
+  const [page, setPage] = useState('Reducer')
   return (
     <div name='container'>
       <Head>React Hook Practice</Head>
       <h1>React Hook Practice</h1>
       <HighlightButton mode={page} setMode={setPage} value='State' />
       <HighlightButton mode={page} setMode={setPage} value='StateApi' />
+      <HighlightButton mode={page} setMode={setPage} value='Reducer' />
       <hr/>
       {
         page === 'State' ?
           <TodoState /> :
           page === 'StateApi' ?
             <TodoStateApi /> :
-            <></>
+            page === 'Reducer' ?
+              <TodoReducer /> :
+              <></>
       }
       <ToastContainer position='top-center' transition={Slide} autoClose={1000} />
     </div>
