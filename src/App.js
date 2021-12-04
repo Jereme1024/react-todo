07e6c9d208a99113d810dd5d3e7fcd10d5a06dd1
Jeremy Hong <jeremy.hong@hp.com>
2021-12-05 00:20:41 +0800

    Add router

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
 import './App.css'
-import { useState, useReducer } from 'react'
+import React, { Suspense } from 'react'
+import { BrowserRouter, Routes, Route, NavLink as Link } from 'react-router-dom'
+import { useReducer } from 'react'
 import { ToastContainer, Slide } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import HighlightButton from './components/HighlightButton'
-import TodoState from './pages/TodoState'
-import TodoStateApi from './pages/TodoStateApi'
-import TodoReducer from './pages/TodoReducer'
-import TodoFirestore from './pages/TodoFirestore'
-import TodoStateAll from './pages/TodoStateAll'
-import TodoStateAllLocalStorage from './pages/TodoStateAllLocalStorage'
 import { initState, reducer } from './vm/todoVm'
 import Context from './store'
 
+const TodoState = React.lazy(() => import('./pages/TodoState'))
+const TodoStateApi = React.lazy(() => import('./pages/TodoStateApi'))
+const TodoReducer = React.lazy(() => import('./pages/TodoReducer'))
+const TodoFirestore = React.lazy(() => import('./pages/TodoFirestore'))
+const TodoStateAll = React.lazy(() => import('./pages/TodoStateAll'))
+const TodoStateAllLocalStorage = React.lazy(() => import('./pages/TodoStateAllLocalStorage'))
+
 function App() {
-  const [page, setPage] = useState('StateAllLocalStorage')
   const store = useReducer(reducer, initState)
 
   return (
     <div name='container'>
       <Context.Provider value={store}>
         <h1>React Hook Practice</h1>
-        <HighlightButton mode={page} setMode={setPage} value='State' />
-        <HighlightButton mode={page} setMode={setPage} value='StateApi' />
-        <HighlightButton mode={page} setMode={setPage} value='Reducer' />
-        <HighlightButton mode={page} setMode={setPage} value='Firestore' />
-        <HighlightButton mode={page} setMode={setPage} value='StateAll' />
-        <HighlightButton mode={page} setMode={setPage} value='StateAllLocalStorage' />
-        <hr/>
-        {
-          page === 'State' ?
-            <TodoState /> :
-            page === 'StateApi' ?
-              <TodoStateApi /> :
-              page === 'Reducer' ?
-                <TodoReducer /> :
-                page === 'Firestore' ?
-                  <TodoFirestore /> :
-                  page === 'StateAll' ?
-                    <TodoStateAll /> :
-                    page === 'StateAllLocalStorage' ?
-                      <TodoStateAllLocalStorage /> :
-                      <></>
-        }
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/basic">Basic</Link>
+              </li>
+              <li>
+                <Link to="/all">All</Link>
+              </li>
+              <li>
+                <Link to="/mockApi">Api</Link>
+              </li>
+              <li>
+                <Link to="/reducer">Reducer</Link>
+              </li>
+              <li>
+                <Link to="/localStorage">LocalStorage</Link>
+              </li>
+              <li>
+                <Link to="/fireStore">FireStore</Link>
+              </li>
+            </ul>
+          </nav>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Routes>
+              <Route path="/" element={<TodoState />} />
+              <Route path="/basic" element={<TodoState />} />
+              <Route path="/all" element={<TodoStateAll />} />
+              <Route path="/mockApi" element={<TodoStateApi />} />
+              <Route path="/reducer" element={<TodoReducer />} />
+              <Route path="/localStorage" element={<TodoStateAllLocalStorage />} />
+              <Route path="/fireStore" element={<TodoFirestore />} />
+              <Route path="*" element={<h1>404 Not Found</h1>} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
         <ToastContainer position='top-center' transition={Slide} autoClose={1000} />
       </Context.Provider>
     </div>
